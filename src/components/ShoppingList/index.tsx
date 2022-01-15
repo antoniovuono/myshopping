3b2f4d7886a3f5ba18981bee591625661e3ea295
Antonio De Vuono <antonio.vuono@corebiz.ag>
2022-01-14 22:25:40 -0300

    Get products from firestore implemented from shopping list component

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { FlatList } from "react-native";
+import firestore from "@react-native-firebase/firestore";
+import { styles } from "./styles";
+import { Product, ProductProps } from "../Product";
 
-import { styles } from './styles';
-import { Product, ProductProps } from '../Product';
-
-import { shoppingListExample } from '../../utils/shopping.list.data';
+import { shoppingListExample } from "../../utils/shopping.list.data";
 
 export function ShoppingList() {
   const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
 
+  useEffect(() => {
+    function bootstrap() {
+      firestore()
+        .collection("products")
+        .onSnapshot((items) => {
+          const data = items.docs.map((documents) => {
+            return {
+              id: documents.id,
+              ...documents.data(),
+            };
+          }) as ProductProps[];
+
+          setProducts(data);
+        });
+    }
+
+    bootstrap();
+  }, []);
+
   return (
     <FlatList
       data={products}
-      keyExtractor={item => item.id}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => <Product data={item} />}
       showsVerticalScrollIndicator={false}
       style={styles.list}
