979359030887b8296a337ab2d7285a08f6426b5a
Antonio De Vuono <antonio.vuono@corebiz.ag>
2022-01-15 18:10:03 -0300

    Upload images to cloud store and cloudstorage installation

--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
-import * as ImagePicker from 'expo-image-picker';
+import React, { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import storage from "@react-native-firebase/storage";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Photo } from "../../components/Photo";
 
-import { Button } from '../../components/Button';
-import { Header } from '../../components/Header';
-import { Photo } from '../../components/Photo';
-
-import { Container, Content, Progress, Transferred } from './styles';
+import { Container, Content, Progress, Transferred } from "./styles";
+import { Alert } from "react-native";
 
 export function Upload() {
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState("");
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
-    if (status == 'granted') {
+    if (status == "granted") {
       const result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         aspect: [4, 4],
@@ -24,7 +25,17 @@ export function Upload() {
         setImage(result.uri);
       }
     }
-  };
+  }
+
+  async function handleUpload() {
+    const fileName = new Date().getTime();
+    const reference = storage().ref(`/images/${fileName}`);
+
+    reference
+      .putFile(image)
+      .then(() => Alert.alert("Upload concluído"))
+      .catch((error) => console.error(error));
+  }
 
   return (
     <Container>
@@ -33,18 +44,11 @@ export function Upload() {
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
 
-        <Button
-          title="Fazer upload"
-          onPress={() => { }}
-        />
+        <Button title="Fazer upload" onPress={handleUpload} />
 
-        <Progress>
-          0%
-        </Progress>
+        <Progress>0%</Progress>
 
-        <Transferred>
-          0 de 100 bytes transferido
-        </Transferred>
+        <Transferred>0 de 100 bytes transferido</Transferred>
       </Content>
     </Container>
   );
