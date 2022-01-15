560d0d5d953f83f99cc744f4fbd66521ca8239a2
Antonio De Vuono <antonio.vuono@corebiz.ag>
2022-01-14 22:50:21 -0300

    Button to delete product and to done toogle component

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,41 +1,54 @@
-import React from 'react';
-
-import { ButtonIcon } from '../ButtonIcon';
-import { Container, Info, Title, Quantity, Options } from './styles';
+import React from "react";
+import firestore from "@react-native-firebase/firestore";
+import { ButtonIcon } from "../ButtonIcon";
+import { Container, Info, Title, Quantity, Options } from "./styles";
+import { Alert } from "react-native";
 
 export type ProductProps = {
   id: string;
   description: string;
   quantity: number;
   done: boolean;
-}
+};
 
 type Props = {
   data: ProductProps;
-}
+};
 
 export function Product({ data }: Props) {
+  function handleDoneTogle() {
+    try {
+      firestore().collection("products").doc(data.id).update({
+        done: !data.done,
+      });
+    } catch (err) {
+      Alert.alert("Nāo foi possível alterar o estado");
+    }
+  }
+
+  function handleDeleteProduct() {
+    try {
+      firestore().collection("products").doc(data.id).delete();
+    } catch (err) {
+      Alert.alert("Nāo foi possível deletar o produto");
+    }
+  }
+
   return (
     <Container>
       <Info>
-        <Title done={data.done}>
-          {data.description}
-        </Title>
+        <Title done={data.done}>{data.description}</Title>
 
-        <Quantity>
-          Quantidade: {data.quantity}
-        </Quantity>
+        <Quantity>Quantidade: {data.quantity}</Quantity>
       </Info>
 
       <Options>
         <ButtonIcon
           icon={data.done ? "undo" : "check"}
+          onPress={handleDoneTogle}
         />
 
-        <ButtonIcon
-          icon="delete"
-          color="alert"
-        />
+        <ButtonIcon icon="delete" color="alert" onPress={handleDeleteProduct} />
       </Options>
     </Container>
   );
