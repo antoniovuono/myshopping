47a4a6ffa35800f0d8fcfd1d331c392b5cdbb34e
Antonio De Vuono <antonio.vuono@corebiz.ag>
2022-01-18 22:22:34 -0300

    User logged tratative implemented

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-
+import auth from "@react-native-firebase/auth";
 import { AppRoutes } from "./app.routes";
 import { SignIn } from "../screens/SignIn";
 
+type User = {
+  uid: string;
+};
+
 export function Routes() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const subscrriber = auth().onAuthStateChanged((userInfo) => {
+      setUser(userInfo);
+    });
+
+    return subscrriber;
+  }, []);
+
   return (
     <NavigationContainer>
-      <SignIn />
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 }
